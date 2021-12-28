13577163f6c0c5f06e3e9d41ff80b031394e51ed
Lynxtickler <25136242+Lynxtickler@users.noreply.github.com>
2021-12-28 15:02:16 +0200

    initial logic distribution to prevent unnecessary lambda re-deploys

--- functions/quote-get.py
+++ functions/quote-get.py
@@ -1,5 +1,12 @@
-import responses
+import shared
 
 
 def lambda_handler(event, context):
-    return responses.get_one_item(event['pathParameters']['id'])
+    item_id = event['pathParameters']['id']
+    try:
+        item = shared.table.get_item(Key={'ID': item_id})['Item']
+    except:
+        item = None
+    if item:
+        return shared.response(code=200, body=item)
+    return shared.create_error(404, 'No such resource.')
