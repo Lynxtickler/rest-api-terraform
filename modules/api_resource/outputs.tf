f79a3a166733e4f9f0b1932d157b766f62c0c8f3
Lynxtickler <25136242+Lynxtickler@users.noreply.github.com>
2025-08-21 02:49:04 +0300

    fix api key requirement not updating by forcing re-deployments

--- modules/api_resource/outputs.tf
+++ modules/api_resource/outputs.tf
@@ -1,3 +1,11 @@
 output "resource_id" {
   value = aws_api_gateway_resource.this.id
 }
+
+output "method_ids" {
+  value = [for method in aws_api_gateway_method.this : method.id]
+}
+
+output "integration_ids" {
+  value = [for integration in aws_api_gateway_integration.this : integration.id]
+}
