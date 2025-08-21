13d84b81ec6a5a22510d5a3ec30eee0fe32d85c1
Lynxtickler <25136242+Lynxtickler@users.noreply.github.com>
2025-08-21 12:49:57 +0300

    minor formatting change

--- api_gateway.tf
+++ api_gateway.tf
@@ -76,7 +76,8 @@ resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
 
   triggers = {
-    redeployment = sha1(jsonencode([aws_api_gateway_rest_api.this.body,
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_rest_api.this.body,
       module.quotes_root.method_ids,
       module.quotes_root.integration_ids,
       module.quotes_sub.method_ids,
